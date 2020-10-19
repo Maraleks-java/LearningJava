d47a752ed920d927d6ed01bee6903b21b55af271
Aleksandr_Markov <aleksandr.markovgit@gmail.com>
2020-10-19 17:47:18 +0300

    Made a way to complete the work in a separate block

--- src/tasks/procedural/refactor/Calculator.java
+++ src/tasks/procedural/refactor/Calculator.java
@@ -25,32 +25,34 @@ public class Calculator {
     public static double getRatioOfNumbers(double a, double b) throws ArithmeticException{
         return a/b;
     }
+    
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        boolean powerButton = true;
-        while (powerButton){
+        boolean processingIsAvailable = true;
+        while (processingIsAvailable){
             byte attempts = 3;
             String input;
             double firstNumber = 0;
             double secondNumber = 0;
-            char mathematicalProcessing = 'd';
+            char userCommand = 0;
             byte stage = 1;
+
+            System.out.println("Press Enter to continue or type stop to exit");
+            input = reader.readLine();
+            if(input.equalsIgnoreCase("stop")){
+                processingIsAvailable = false;
+                break;
+            }
+
             while (attempts > 0) {
-                if(attempts == 0) {
-                    break;
-                }
                 try {
 
                     if(stage == 1 || stage == 3) {
-                        System.out.println("Enter the number. To exit - enter \"stop\"");
+                        System.out.println("Enter the number.");
                         input = reader.readLine();
-                        if(input.equalsIgnoreCase("stop")){
-                            powerButton = false;
-                            break;
-                        }
                         if(stage == 1) {
                             firstNumber = Double.parseDouble(input);
                         }
@@ -63,37 +65,36 @@ public class Calculator {
                     if(stage == 2) {
                         System.out.println( "Enter the command [+ , -, *, /]");
                         input = reader.readLine();
+                        userCommand = input.charAt(0);
                         char[] commands = {'+', '-', '*', '/' };
                         for (int i = 0; i < commands.length; i++) {
-                            if(commands[i] == input.charAt(0)) {
-                                mathematicalProcessing = commands[i];
+                            if(commands[i] == userCommand) {
                                 stage++;
                                 break;
                             }
                         }
-                        if(mathematicalProcessing == 'd') {
+                        if(userCommand == 'd') {
                             attempts--;
                             System.out.println( "You entered the wrong command! Attempts: " + attempts);
                         }
                     }
 
-                    if(stage > 3) {
-                        if(mathematicalProcessing == '+') {
-                            System.out.println(Calculator.getSumOfNumbers(firstNumber, secondNumber));
-                            break;
+                    if(stage == 4) {
+                        switch (userCommand) {
+                            case '+':
+                                System.out.println(Calculator.getSumOfNumbers(firstNumber, secondNumber));
+                                break;
+                            case '-':
+                                System.out.println(Calculator.getDifferenceOfNumbers(firstNumber, secondNumber));
+                                break;
+                            case '*':
+                                System.out.println(Calculator.getProductOfNumbers(firstNumber, secondNumber));
+                                break;
+                            case '/':
+                                System.out.println(Calculator.getRatioOfNumbers(firstNumber, secondNumber));
+                                break;
                         }
-                        if(mathematicalProcessing == '-') {
-                            System.out.println(Calculator.getDifferenceOfNumbers(firstNumber, secondNumber));
-                            break;
-                        }
-                        if(mathematicalProcessing == '*') {
-                            System.out.println(Calculator.getProductOfNumbers(firstNumber, secondNumber));
-                            break;
-                        }
-                        if(mathematicalProcessing == '/') {
-                            System.out.println(Calculator.getRatioOfNumbers(firstNumber, secondNumber));
-                            break;
-                        }
+                        break;
                     }
 
                 } catch (NumberFormatException e) {
