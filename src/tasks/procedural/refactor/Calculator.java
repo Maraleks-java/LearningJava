33b607fbce7c2b070f5b524b98d34b4ba7cf78fd
Aleksandr_Markov <aleksandr.markovgit@gmail.com>
2020-10-21 13:20:39 +0300

    Refactoring

--- src/tasks/procedural/refactor/Calculator.java
+++ src/tasks/procedural/refactor/Calculator.java
@@ -14,47 +14,6 @@ public class Calculator {
 
     private final static char[] MATH_COMMANDS = {'+', '-', '*', '/' };
 
-    public static double getSumOfNumbers(double a, double b) {
-        return a+b;
-    }
-
-    public static double getDifferenceOfNumbers(double a, double b) {
-        return a-b;
-    }
-
-    public static double getProductOfNumbers(double a, double b) {
-        return a*b;
-    }
-
-    public static double getRatioOfNumbers(double a, double b) throws ArithmeticException{
-        return a/b;
-    }
-
-    public static boolean isMathCommand(char userCommand) {
-        for (int mathCommand: MATH_COMMANDS) {
-            if(userCommand == mathCommand) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean сompletionСheck(String userCommand) {
-        if(userCommand.equalsIgnoreCase("stop")){
-            return true;
-        }
-        return false;
-    }
-
-    public static void message(String message) {
-        String formattedDateTime = getFormattedDate(LocalDateTime.now());
-        System.out.println(formattedDateTime + " " + message);
-    }
-
-    private static String getFormattedDate(LocalDateTime dateTime) {
-        return dateTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm:ss"));
-    }
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -79,9 +38,9 @@ public class Calculator {
             userCommand = 0;
             stage = 1;
 
-            message("Press Enter to continue or enter stop to exit");
+            displayMessage("Press Enter to continue or enter stop to exit");
             input = reader.readLine();
-            if(сompletionСheck(input)){
+            if(isStopCommand(input)){
                 processingIsAvailable = false;
                 break;
             }
@@ -90,7 +49,7 @@ public class Calculator {
                 try {
 
                     if(stage == ENTER_FIRST_NUMBER || stage == ENTER_SECOND_NUMBER) {
-                        message("Enter the number.");
+                        displayMessage("Enter the number.");
                         input = reader.readLine();
                         if(stage == ENTER_FIRST_NUMBER) {
                             firstNumber = Double.parseDouble(input);
@@ -101,30 +60,35 @@ public class Calculator {
                     }
 
                     if(stage == ENTER_COMMAND) {
-                        message("Enter the command [+ , -, *, /]");
+                        displayMessage("Enter the command [+ , -, *, /]");
                         input = reader.readLine();
                         userCommand = input.charAt(0);
-                        if(Calculator.isMathCommand(userCommand)){
+                        if(isMathCommand(userCommand)){
                             stage++;
                         } else {
                             attempts--;
-                            message("You entered the wrong command! Attempts: " + attempts);
+                            displayMessage("You entered the wrong command! Attempts: " + attempts);
                         }
                     }
 
                     if(stage == PERFORM_CALCULATION) {
+                        double result;
                         switch (userCommand) {
                             case '+':
-                                message(String.valueOf(getSumOfNumbers(firstNumber, secondNumber)));
+                                result = getSumOfNumbers(firstNumber, secondNumber);
+                                displayMessage(String.valueOf(result));
                                 break;
                             case '-':
-                                message(String.valueOf(getDifferenceOfNumbers(firstNumber, secondNumber)));
+                                result = getDifferenceOfNumbers(firstNumber, secondNumber);
+                                displayMessage(String.valueOf(result));
                                 break;
                             case '*':
-                                message(String.valueOf(getProductOfNumbers(firstNumber, secondNumber)));
+                                result = getProductOfNumbers(firstNumber, secondNumber);
+                                displayMessage(String.valueOf(result));
                                 break;
                             case '/':
-                                message(String.valueOf(getRatioOfNumbers(firstNumber, secondNumber)));
+                                result = getRatioOfNumbers(firstNumber, secondNumber);
+                                displayMessage(String.valueOf(result));
                                 break;
                         }
                         break;
@@ -132,9 +96,51 @@ public class Calculator {
 
                 } catch (NumberFormatException e) {
                     attempts--;
-                    message("You didn't enter a number. Try again! Attempts: " + attempts);
+                    displayMessage("You didn't enter a number. Try again! Attempts: " + attempts);
                 }
             }
         }
     }
+
+    public static void displayMessage(String message) {
+        String formattedDateTime = getFormattedDate(LocalDateTime.now());
+        System.out.println(formattedDateTime + " " + message);
+    }
+
+    private static String getFormattedDate(LocalDateTime dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm:ss"));
+    }
+
+    public static boolean isStopCommand(String userCommand) {
+        if(userCommand.equalsIgnoreCase("stop")){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isMathCommand(char userCommand) {
+        for (int mathCommand: MATH_COMMANDS) {
+            if(userCommand == mathCommand) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static double getSumOfNumbers(double a, double b) {
+        return a+b;
+    }
+
+    public static double getDifferenceOfNumbers(double a, double b) {
+        return a-b;
+    }
+
+    public static double getProductOfNumbers(double a, double b) {
+        return a*b;
+    }
+
+    public static double getRatioOfNumbers(double a, double b) throws ArithmeticException{
+        return a/b;
+    }
+
 }
