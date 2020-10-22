77b31d6fb612cf99c2a0c3089b4448e08c240a2d
Aleksandr_Markov <aleksandr.markovgit@gmail.com>
2020-10-22 22:30:41 +0300

    Described enumeration of displayed messages.

---
+++ src/tasks/oop/task3/view/Messages.java
@@ -0,0 +1,22 @@
+package tasks.oop.task3.view;
+
+public enum Messages {
+
+    CONTINUE_OR_STOP("Press Enter to continue or enter stop to exit"),
+    ENTER_FIRST_NUMBER("Enter the first number."),
+    ENTER_SECOND_NUMBER("Enter the second number."),
+    ENTER_COMMAND("Enter the command [+ , -, *, /]"),
+    DIVISION_BY_ZERO("Division by zero is not possible!"),
+    NUMBER_INPUT_ERROR("You didn't enter a number. Try again! Attempts: "),
+    COMMAND_INPUT_ERROR("You entered the wrong command! Attempts: ");
+
+    private String text;
+    Messages(String text) {
+        this.text = text;
+    }
+
+    public String getMessageText() {
+        return text;
+    }
+
+}
