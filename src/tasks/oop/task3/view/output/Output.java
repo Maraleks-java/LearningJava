3024dfbc9886b45caba0b35be87d209a3af0c546
Aleksandr_Markov <aleksandr.markovgit@gmail.com>
2021-03-06 13:38:55 +0300

    An example of javadoc design.

--- src/tasks/oop/task3/view/output/Output.java
+++ src/tasks/oop/task3/view/output/Output.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * @author Alexandr Markov
  * @version 1.0
+ * @author Alexandr Markov
  */
 public class Output {
     /**
+     * Default constructor.
      */
     public Output() {
 
@@ -16,12 +17,18 @@ public class Output {
 
     /**
      * @param message
+     * The method accepts the text of the message.
+     * Sets the date and time of the output and prints to the console.
      */
     public void displayMessage(final String message) {
         String formattedDateTime = getFormattedDate(LocalDateTime.now());
         System.out.println(formattedDateTime + " " + message);
     }
 
+    /**
+     * @param dateTime
+     * @return String representation of date and time.
+     */
     private static String getFormattedDate(final LocalDateTime dateTime) {
         return dateTime.format(DateTimeFormatter.
                                ofPattern("yyyy.MM.dd hh:mm:ss"));
