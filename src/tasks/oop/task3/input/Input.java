167a2e802ea9f2810437989bb10206f54ed58e8d
Aleksandr_Markov <aleksandr.markovgit@gmail.com>
2021-03-23 15:28:15 +0300

    Rewrote method names.

--- src/tasks/oop/task3/input/Input.java
+++ src/tasks/oop/task3/input/Input.java
@@ -1,13 +1,23 @@
+/*
+ * %W% %E% Aleksandr Markov
+ *
+ */
 package tasks.oop.task3.input;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * @version 1.0
+ * @author Alexandr Markov
+ */
 public class Input {
+
     /**
      */
     private BufferedReader reader;
+
     /**
      */
     public Input() {
@@ -19,8 +29,8 @@ public class Input {
      * @throws IOException
      * @throws NumberFormatException
      */
-    public double readsNumberFromKeyboard() throws
-            IOException, NumberFormatException {
+    public double readOnlyNumbersFromKeyboard() throws IOException,
+            NumberFormatException {
         String input = reader.readLine();
         double number = 0;
         number = Double.parseDouble(input);
@@ -31,7 +41,7 @@ public class Input {
      * @return Returns the code of the first custom character.
      * @throws IOException
      */
-    public double readsCodeCommandFromKeyboard() throws IOException {
+    public double readKeyboardCommandCode() throws IOException {
         return reader.readLine().charAt(0);
     }
 
