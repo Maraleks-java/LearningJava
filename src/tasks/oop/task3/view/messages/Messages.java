ffb1abf452454b4e25a33234eba4c6e693a6118a
Aleksandr_Markov <aleksandr.markovgit@gmail.com>
2021-03-28 23:42:27 +0300

    Fixed javadoc

--- src/tasks/oop/task3/view/messages/Messages.java
+++ src/tasks/oop/task3/view/messages/Messages.java
@@ -9,27 +9,27 @@ package tasks.oop.task3.view.messages;
 public enum Messages {
 
     /**
-     * Object with a text message.
+     * Text message.
      */
     CONTINUE_OR_STOP("Press Enter to continue or enter stop to exit"),
     /**
-     * Object with a text message.
+     * Text message.
      */
     ENTER_FIRST_NUMBER("Enter the first number."),
     /**
-     * Object with a text message.
+     * Text message.
      */
     ENTER_SECOND_NUMBER("Enter the second number."),
     /**
-     * Object with a text message.
+     * Text message.
      */
     ENTER_COMMAND("Enter the command [+ , -, *, /]"),
     /**
-     * Object with a text message.
+     * Text message.
      */
     NUMBER_INPUT_ERROR("You didn't enter a number. Try again! Attempts: "),
     /**
-     * Object with a text message.
+     * Text message.
      */
     COMMAND_INPUT_ERROR("You entered the wrong command! Attempts: ");
 
